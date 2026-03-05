0bcbbe03f0b55607b4d5370135817f29cd8a101e
thebenstenator <thebenstenator@gmail.com>
2026-03-05 10:45:09 -0700

    Create media query for tablet

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,10 +4,7 @@ import "./Hero.css";
 function Hero() {
   return (
     <section className="hero">
-      <h1 className="hero__title">
-        What's going on in <br />
-        the world?
-      </h1>
+      <h1 className="hero__title">What's going on in the world?</h1>
       <p className="hero__subtitle">
         Find the latest news on any topic and save them in your personal
         account.
